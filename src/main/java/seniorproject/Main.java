de73d2b78e677f95ee57aa861029ce94641275bd
Austin Egan <austinegan97@gmail.com>
2022-02-17 18:33:14 -0500

    changed main and pom to work with SWT

--- src/main/java/seniorproject/Main.java
+++ src/main/java/seniorproject/Main.java
@@ -1,9 +1,70 @@
 package seniorproject;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Shell;
+import org.eclipse.swt.widgets.Text;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.widgets.Button;
 
 public class Main {
-public static void main(String[]args) {
-	System.out.println("pls work");
-	System.out.println("pls work 2");
-	System.out.println("Please work 3");
-}
+
+	protected Shell shell;
+	private Text text;
+	private Text text_1;
+
+	/**
+	 * Launch the application.
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		try {
+			Main window = new Main();
+			window.open();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * Open the window.
+	 */
+	public void open() {
+		Display display = Display.getDefault();
+		createContents();
+		shell.open();
+		shell.layout();
+		while (!shell.isDisposed()) {
+			if (!display.readAndDispatch()) {
+				display.sleep();
+			}
+		}
+	}
+
+	/**
+	 * Create contents of the window.
+	 */
+	protected void createContents() {
+		shell = new Shell();
+		shell.setSize(450, 300);
+		shell.setText("SWT Application");
+		
+		text = new Text(shell, SWT.BORDER);
+		text.setBounds(212, 74, 76, 21);
+		
+		text_1 = new Text(shell, SWT.BORDER);
+		text_1.setBounds(212, 135, 76, 21);
+		
+		Label lblNewLabel = new Label(shell, SWT.NONE);
+		lblNewLabel.setBounds(87, 74, 55, 15);
+		lblNewLabel.setText("Username");
+		
+		Label lblNewLabel_1 = new Label(shell, SWT.NONE);
+		lblNewLabel_1.setBounds(87, 135, 55, 15);
+		lblNewLabel_1.setText("Password");
+		
+		Button btnLogin = new Button(shell, SWT.NONE);
+		btnLogin.setBounds(144, 204, 75, 25);
+		btnLogin.setText("Login");
+
+	}
 }
