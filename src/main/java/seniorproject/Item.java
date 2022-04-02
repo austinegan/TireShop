1e952a8e7a12984bf27b8aca53d90c240b1fb539
Austin Egan <austinegan97@gmail.com>
2022-04-02 12:36:15 -0400

    Item constructor

--- src/main/java/seniorproject/Item.java
+++ src/main/java/seniorproject/Item.java
@@ -13,4 +13,23 @@ int width;
 int size;
 int aspectRatio;
 int diameter;
+
+public Item(int id, String brand, int modelNum, double salePrice, double purchasePrice, int count, int width, int size,
+		int aspectRatio, int diameter) {
+	super();
+	this.id = id;
+	this.brand = brand;
+	this.modelNum = modelNum;
+	this.salePrice = salePrice;
+	this.purchasePrice = purchasePrice;
+	this.count = count;
+	this.width = width;
+	this.size = size;
+	this.aspectRatio = aspectRatio;
+	this.diameter = diameter;
+}
+
+public void print() {	//for testing contents of an item
+	
+}
 }
