6305c00232ab3d02e817de85ebbfae1ef1e1535b
Austin Egan <austinegan97@gmail.com>
2022-04-03 21:47:24 -0400

    i don';t wanna

--- src/main/java/seniorproject/SpendMoney.java
+++ src/main/java/seniorproject/SpendMoney.java
@@ -1,7 +1,9 @@
 package seniorproject;
 import com.squareup.square.Environment;
 import com.squareup.square.SquareClient;
-
+import com.squareup.square.models.*;
+import com.squareup.square.api.*;
+import com.squareup.square.*;
 public class SpendMoney {
 	
 	
@@ -11,7 +13,32 @@ public class SpendMoney {
 		  .accessToken("EAAAEA0HoegiPjZ_NwZVzYspp_tmBy_hFlmZDMqHcP2tBpjDpqkbjyon5xR_NrKD")
 		  .build();
 
+		Address address = new Address.Builder()
+				   .addressLine1("1455 Market St")
+				   .addressLine2("San Francisco, CA 94103")
+				   .build();
 
+				CreateCustomerRequest request = new CreateCustomerRequest.Builder()
+				    .idempotencyKey("12345")
+				    .givenName("JohnJava2")
+				    .familyName("DoeJava2")
+				    .address(address)
+				    .build();
+				//CustomersApi customersApi;
+				CustomersApi customersApi = client.getCustomersApi();
+				customersApi.createCustomerAsync(request)
+				    .thenAccept(result -> {
+				        System.out.printf("customer created:\n Given name =  %s Family name = %s",
+				            result.getCustomer().getGivenName(),
+				            result.getCustomer().getFamilyName());
+				    })
+				    .exceptionally(exception -> {
+				        System.out.println("Failed to make the request");
+				        System.out.println(String.format("Exception: %s", 
+				            exception.getMessage()));
+				        return null;
+				    })
+				    .join();
 	}
 
 
